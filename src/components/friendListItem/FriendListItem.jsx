6f08fc0b8eff9a674c1cc8fc9fb07c8b572ccd08
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-10-17 22:34:14 +0200

    Final styles added

--- src/components/friendListItem/FriendListItem.jsx
+++
@@ -1,9 +0,0 @@
-export function FriendListItem({ avatar, name, isOnline }) {
-  return (
-    <li class="item">
-      <span class="status">{isOnline}</span>
-      <img class="avatar" src={avatar} alt="User avatar" width="48" />
-      <p class="name">{name}</p>
-    </li>
-  );
-}
